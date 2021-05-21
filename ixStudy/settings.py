5c3d7a4e6e6ded0d5a7d25ae764df68e5d6bbf84
Obsir <Obser47@outlook.com>
2021-05-21 16:35:23 +0800

    用户表创建

--- ixStudy/settings.py
+++ ixStudy/settings.py
@@ -80,8 +80,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ixstudy',
         'HOST': '127.0.0.1',
-        'PORT': 'root',
-        'PASSWORD': '123456'
+        'PORT': 3306,
+        'USER': 'root',
+        'PASSWORD': 'cx951227'
     }
 }
 
